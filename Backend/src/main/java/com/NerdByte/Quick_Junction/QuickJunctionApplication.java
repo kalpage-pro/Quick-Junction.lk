3e6d538903d68f666e210395d12a782870eed622
KALPAGE <minidu099@gmail.com>
2025-10-07 15:09:50 +0530

    initializr

---
+++ Backend/src/main/java/com/NerdByte/Quick_Junction/QuickJunctionApplication.java
@@ -0,0 +1,13 @@
+package com.NerdByte.Quick_Junction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickJunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickJunctionApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/NerdByte/Quick_Junction/QuickJunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NerdByte.Quick_Junction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickJunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
