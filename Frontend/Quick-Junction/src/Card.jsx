fc9928df2748cd5c93822299f1189ebe247fd27b
KALPAGE <minidu099@gmail.com>
2025-10-09 15:56:58 +0530

    card view

---
+++ Frontend/Quick-Junction/src/Card.jsx
@@ -0,0 +1,13 @@
+
+function Card(){
+
+    return(
+        <div className="card">
+            <img src="images\NerdByte.jpg" alt="" className="profile picture" />
+            <h2 className="card-text">NerdByte</h2>
+            <p className="card_text">Make Youtube videos and programming</p>
+        </div>
+    );
+}
+
+export default Card;
